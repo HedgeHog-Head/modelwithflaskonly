744d6cb2bc285131c5308263a8b24d63bd4da309
unknown <konsaigao81@gmail.com>
2020-10-27 21:10:30 +0800

    test

--- main.py
+++ main.py
@@ -113,5 +113,5 @@ def predict_api():
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run(host="127.0.0.1",port=8080,debug=True)
+    app.run(debug=True)
 
