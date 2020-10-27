237a6209a7deaac650b53231fdccc8e99b3f1c39
unknown <konsaigao81@gmail.com>
2020-10-27 23:13:54 +0800

    500

--- main.py
+++ main.py
@@ -109,7 +109,10 @@ def predict_api():
 
     return jsonify(inference_payload['value'])
 
-
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify(error=str(e)),500
+    
 if __name__ == '__main__':
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
